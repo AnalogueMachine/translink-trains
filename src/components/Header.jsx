984dcf6952622a2f90ca9910e83c42553c0cdb0a
ML <test@test.com>
2019-05-26 19:59:01 +0100

    New component plus tests

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+
+const Header = () => {
+  return (
+    <Navbar bg='light' expand='lg'>
+      <Navbar.Brand href='/'>Translink Trains</Navbar.Brand>
+      <Navbar.Toggle aria-controls='basic-navbar-nav' />
+      <Navbar.Collapse id='basic-navbar-nav'>
+        <Nav className='mr-auto'>
+          <Nav.Link href='https://github.com/AnalogueMachine'>GitHub</Nav.Link>
+          <Nav.Link href='http://twitter.com/AnalogueMachine'>Twitter</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+export default Header;
---
+++ src/components/__tests__/Header.spec.js
@@ -0,0 +1,119 @@
+import { shallow } from 'enzyme';
+import Header from '../Header';
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+
+const collapseId = 'basic-navbar-nav';
+
+describe('Header', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<Header />);
+  });
+
+  it('should return a Navbar component', () => {
+    expect(wrapper.type()).toBe(Navbar);
+  });
+
+  it('should have the correct properties', () => {
+    expect(wrapper.props().bg).toEqual('light');
+    expect(wrapper.props().expand).toEqual('lg');
+  });
+
+  describe('Brand element', () => {
+    let brandElement;
+
+    beforeEach(() => {
+      brandElement = wrapper.props().children[0];
+    })
+
+    it('should be the correct type', () => {  
+      expect(brandElement.type).toBe(Navbar.Brand);
+    });
+
+    it('should have the appropriate text', () => {
+      expect(brandElement.props.children).toBe('Translink Trains');
+    });
+
+    it('should contain a link to itself', () => {
+      expect(brandElement.props.href).toBe('/');
+    });
+  });
+
+  describe('Collapse toggle element', () => {
+    let toggle;
+
+    beforeEach(() => {
+      toggle = wrapper.props().children[1];
+    });
+
+    it('should be the correct type', () => {
+      expect(toggle.type).toBe(Navbar.Toggle);
+    });
+
+    it('should toggle the correct element using its id', () => {
+      expect(toggle.props).toHaveProperty('aria-controls', collapseId);
+    });
+  });
+
+  describe('Collapse element', () => {
+    let collapseElement;
+
+    beforeEach(() => {
+      collapseElement = wrapper.props().children[2];
+    });
+
+    it('should be the correct type of element', () => {
+      expect(collapseElement.type).toBe(Navbar.Collapse)
+    });
+
+    it('should have an id that matches the toggle aria control', () => {
+      expect(collapseElement.props.id).toEqual(collapseId);
+    });
+
+    describe('Nav element', () => {
+      let navElement;
+
+      beforeEach(() => {
+        navElement = collapseElement.props.children;
+      });
+
+      it('should be a Nav element', () => {
+        expect(navElement.type).toBe(Nav);
+      });
+
+      it('should be have the correct style class', () => {
+        expect(navElement.props.className).toBe('mr-auto');
+      });
+
+      describe('Nav links', () => {
+        let links;
+
+        beforeEach(() => {
+          links = navElement.props.children;
+        });
+
+        it('should contain two links', () => {
+          expect(links.length).toEqual(2);
+        });
+
+        it('should be the correct element in all cases', () => {
+          links.forEach((link) => {
+            expect(link.type).toBe(Nav.Link);
+          });
+        });
+        
+        it('should have a link to GitHub as the first link', () => {
+          expect(links[0].props.href).toBe("https://github.com/AnalogueMachine");
+          expect(links[0].props.children).toBe('GitHub');
+        });
+
+        it('should have a link to Twitter as the second link', () => {
+          expect(links[1].props.href).toBe("http://twitter.com/AnalogueMachine");
+          expect(links[1].props.children).toBe('Twitter');
+        });
+      });
+    });
+  });
+});
