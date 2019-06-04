3fc268030690b12664a0df8bfb933b87df1e5733
AnalogueMachine <bunglenutter@gmail.com>
2019-06-04 22:13:08 +0100

    Implements Heroku proxy calls

--- src/services/stationServices.js
+++ src/services/stationServices.js
@@ -3,19 +3,23 @@ import { xml2json } from 'xml-js';
 export const getStations = async () => {
   let stationsArray;
     // cors.io version - only use if cors.io is working
-    await fetch("https://cors.io/?https://apis.opendatani.gov.uk/translink/")
-      .then(results => {
-        return results.json();
-      })
-      .then(data => {
-        stationsArray = data.stations;
-      });
+    // await fetch("https://cors.io/?https://apis.opendatani.gov.uk/translink/")
+    //   .then(results => {
+    //     return results.json();
+    //   })
+    //   .then(data => {
+    //     stationsArray = data.stations;
+    //   });
 
-    // Proxy version - proxy must be running
+    // Lroxy version - proxy must be running
     // await fetch("/stations")
     //   .then(results => { return results.json(); })
     //   .then(data => { stationsArray = data; });
-    
+
+    // Heroku proxy version
+    await fetch("http://translink-proxy.herokuapp.com/stations")
+      .then(results => { return results.json(); })
+      .then(data => { stationsArray = data; });
 
     return stationsArray;
 }
@@ -24,34 +28,39 @@ export const getStationInformation = async (stationCode) => {
   let stationInformation;
 
   // cors.io version - only use if cors.io is working
-  await fetch(
-    `https://cors.io/?https://apis.opendatani.gov.uk/translink/${stationCode}.xml`,
-    {
-      method: "GET",
-      headers: {
-        Accept: "application/xml"
-      }
-    }
-  ).then(response => {
-      return response.text();
-    }
-  ).then(xml => {
-      // console.log(xml);
-      stationInformation = JSON.parse(
-        xml2json(xml, {
-          compact: true,
-          textKey: "_",
-          attributesKey: "$",
-          commentKey: "value"
-        })
-      );
-    }
-  );
+  // await fetch(
+  //   `https://cors.io/?https://apis.opendatani.gov.uk/translink/${stationCode}.xml`,
+  //   {
+  //     method: "GET",
+  //     headers: {
+  //       Accept: "application/xml"
+  //     }
+  //   }
+  // ).then(response => {
+  //     return response.text();
+  //   }
+  // ).then(xml => {
+  //     // console.log(xml);
+  //     stationInformation = JSON.parse(
+  //       xml2json(xml, {
+  //         compact: true,
+  //         textKey: "_",
+  //         attributesKey: "$",
+  //         commentKey: "value"
+  //       })
+  //     );
+  //   }
+  // );
 
   // Local call (proxy must be running)
   // await fetch(`/station/${stationCode}`)
   //   .then(response => { return response.json() })
   //   .then(stationInfo => { stationInformation = stationInfo });
+
+  // Heroku proxy version
+   await fetch(`http://translink-proxy.herokuapp.com/station/${stationCode}`)
+    .then(response => { return response.json() })
+    .then(stationInfo => { stationInformation = stationInfo });
 
   return stationInformation;
 }
